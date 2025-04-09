fc3ed8fc864188503eb9e08d82a101d986e2b9dd
Dev-webp <srichain18@gmail.com>
2025-04-09 13:34:42 +0530

    Describe

--- app/irelandinvestorvisa/Footer.jsx
+++ app/irelandinvestorvisa/Footer.jsx
@@ -5,46 +5,42 @@ import { FaWhatsapp } from 'react-icons/fa';
 const Footer = () => {
   return (
     <>
-    <footer className="bg-gradient-to-r from-gray-800 via-gray-700 to-gray-900 text-white py-12">
+    <footer className="bg-gradient-to-r from-gray-900  to-gray-800 text-white py-12">
     <div className="max-w-7xl mx-auto px-5 text-center md:text-left border-b border-gray-700 mb-10">
         {/* Grid for Content */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-8 mb-2">
             {/* Navigation */}
             <div>
                 <h3 className="text-lg font-bold text-orange-500 mb-1 md:mb-4 lg:mb-2 text-center lg:text-left uppercase">About Us</h3>
-                <ul className="space-y-2 text-center lg:text-left mb-6 md:mb-0 lg:mb-0">
-                    <li><a href="#" className="hover-underline">Services</a></li>
-                    <li><a href="#" className="hover-underline">Our CEO Message</a></li>
-                    <li><a href="#" className="hover-underline">Our Network</a></li>
-                    <li><a href="#" className="hover-underline">Careers</a></li>
+                <ul className="space-y-2 text-center ml-2 lg:text-left mb-6 md:mb-0 lg:mb-0">
+                    <li><a href="services" className="hover-underline">Services</a></li>
+                    <li><a href="/" className="hover-underline">Our CEO Message</a></li>
+                    <li><a href="/" className="hover-underline">Our Network</a></li>
+                    <li><a href="/" className="hover-underline">Careers</a></li>
                 </ul>
             </div>
             {/* Services */}
             <div>
                 <h3 className="text-lg font-bold text-orange-500 mb-1 md:mb-4 lg:mb-2 text-center lg:text-left uppercase">Our Blogs</h3>
                 <ul className="space-y-2 text-center lg:text-left mb-6 md:mb-0 lg:mb-0">
-                    <li><a href="#" className="hover-underline">Canada Immigration</a></li>
-                    <li><a href="#" className="hover-underline">Australia Immigration</a></li>
-                    <li><a href="#" className="hover-underline">USA Immigration</a></li>
-                    <li><a href="#" className="hover-underline">Germany Immigration</a></li>
-                    <li><a href="#" className="hover-underline">Germany Jobs</a></li>
+                    <li><a href="/migrate/canada" className="hover-underline">Canada Immigration</a></li>
+                    <li><a href="/migrate/australia" className="hover-underline">Australia Immigration</a></li>
+                    <li><a href="/migrate/united-states" className="hover-underline">USA Immigration</a></li>
+                    <li><a href="/migrate/germany" className="hover-underline">Germany Immigration</a></li>
+                    <li><a href="/migrate/germany/work-visa" className="hover-underline">Germany Jobs</a></li>
                 </ul>
             </div>
 
              
             <div>
     <h3 className="text-lg  font-bold text-orange-500 mb-1 md:mb-4 lg:mb-2 text-center lg:text-left uppercase">Settle your future in Abroad</h3>
-    <ul className="space-y-1 text-center lg:text-left mb-6 md:mb-0 lg:mb-0">
-        <li><a href="#" className="hover-underline">Settle in Canada</a></li>
-        <li><a href="#" className="hover-underline">Settle in Australia</a></li>
-        <li><a href="#" className="hover-underline">Settle in Germany</a></li>
-        <li><a href="#" className="hover-underline">Settle in UK</a></li>
-        <li><a href="#" className="hover-underline">Settle in USA</a></li>
-        {/* <li><a href="#" className="hover-underline">Settle in New Zealand</a></li>
-        <li><a href="#" className="hover-underline">Settle in South Africa</a></li>
-        <li><a href="#" className="hover-underline">Settle in Ireland</a></li>
-        <li><a href="#" className="hover-underline">Settle in UAE</a></li>
-        <li><a href="#" className="hover-underline">Settle in Denmark</a></li> */}
+    <ul className="space-y-1 text-center lg:text-left ml-2 mb-6 md:mb-0 lg:mb-0">
+        <li><a href="/prvisas/canadapr" className="hover-underline">Settle in Canada</a></li>
+        <li><a href="/prvisas/australiapr" className="hover-underline">Settle in Australia</a></li>
+        <li><a href="/prvisas/germanybluecard" className="hover-underline">Settle in Germany</a></li>
+        <li><a href="/prvisas/ukpr" className="hover-underline">Settle in UK</a></li>
+        <li><a href="/prvisas/usagreencard" className="hover-underline">Settle in USA</a></li>
+        
     </ul>
 </div>
 
@@ -52,12 +48,12 @@ const Footer = () => {
 <div>
                 <h3 className="text-lg font-bold text-orange-500 mb-1 md:mb-4 lg:mb-2 text-center lg:text-left uppercase">Other Services</h3>
                 <ul className="space-y-2 text-center lg:text-left mb-6 md:mb-0 lg:mb-0">
-                    <li><a href="#" className="hover-underline">Free Eligibility Check</a></li>
-                    <li><a href="#" className="hover-underline">Free Counselling</a></li>
-                    <li><a href="#" className="hover-underline">Free Assessment Online</a></li>
-                    <li><a href="#" className="hover-underline">Fast and Easy Process</a></li>
+                    <li><a href="/assessment" className="hover-underline">Free Eligibility Check</a></li>
+                    <li><a href="/" className="hover-underline">Free Counselling</a></li>
+                    <li><a href="/assessment" className="hover-underline">Free Assessment Online</a></li>
+                    <li><a href="/" className="hover-underline">Fast and Easy Process</a></li>
                     <li><a href="#" className="hover-underline">Professional Job Assistance</a></li>
-                    <li><a href="#" className="hover-underline">High Quality Resume Marketing</a></li>
+                    <li><a href="/resume-marketing" className="hover-underline">High Quality Resume Marketing</a></li>
                 </ul>
             </div>
 
@@ -78,11 +74,11 @@ const Footer = () => {
                     <a href="https://www.linkedin.com/company/vjc-overseas/" className="group">
                         <FaLinkedin className="text-white group-hover:text-orange-500" />
                     </a>
-                    <a href="https://www.youtube.com/@vjcoverseas9434" className="group">
+                    <a href="https://www.youtube.com/@Vjcoverseas" className="group">
                         <FaYoutube className="text-white group-hover:text-orange-500" />
                     </a>
                 </div>
-                <div className="flex flex-col items-center justify-center md:items-start mb-6 md:mb-0 mt-10 w-full md:w-auto ml-0 tablet:ml-20 md:ml-0 lg:ml-0">
+                <div className="flex flex-col items-center justify-center md:items-start mb-6 md:mb-0 mt-6 w-full md:w-auto ml-0 tablet:ml-20 md:ml-0 lg:ml-0">
             <p className="text-gray-200 font-medium">Email : <a href="mailto:info@vjcoverseas.com" className="hover:text-saffron">info@vjcoverseas.com</a></p>
             <p className="mt-2 font-medium text-gray-200 ml-0 tablet:ml-2 md:ml-0 lg:ml-0">Contact : <a href="tel:+919160449000" className="hover:text-saffron">+91 9160449000</a></p>
           </div>
@@ -96,9 +92,12 @@ const Footer = () => {
         {/* Address and Contact Section */}
         <div className="flex flex-col md:flex-row justify-between items-center text-center md:text-left space-y-8 md:space-y-0 md:space-x-8">
              {/* Hyderabad Address */}
-        <div className="mb-8 md:mb-0 text-center md:w-1/3">
-            <h3 className="text-lg font-bold uppercase mb-2 text-orange-500">VJC Overseas (Hyderabad)</h3>
-            <p>62/A, Ground Floor,</p>
+             <div className="mb-8 md:mb-0 text-center md:w-1/3">
+  <h3 className="text-lg font-bold uppercase mb-1 text-orange-500">
+    VJC Overseas
+  </h3>
+  <p className="text-lg text-orange-500 font-bold -mt-2 ">(HYDERABAD)</p>
+            <p className='mt-2'>62/A, Ground Floor,</p>
             <p>Sundari Reddy Bhavan, Fresh Mart,</p>
             <p>Vengalrao Nagar, Sanjeeva Reddy Nagar,</p>
             <p>Hyderabad, Telangana, 500038</p>
@@ -118,16 +117,20 @@ const Footer = () => {
         </div>
 
            {/* USA Address */}
-        <div className="flex flex-col items-center md:w-1/3">
-            <h3 className="text-lg font-bold uppercase mb-2 text-orange-500">VJC Overseas (USA)</h3>
-            <p>7200 Preston Rd, Plano,</p>
-            <p>TX 75024, USA</p>
-            <p className="mt-2 font-semibold">
-                <a href="tel:+13322012159" className="flex items-center space-x-2 hover:text-saffron">
-                    <FaWhatsapp size={25} /> 
-                    <span>: +1 3322012159</span>
-                </a>
-            </p>
+           <div className="mb-8 md:mb-0 text-center md:w-1/3">
+  <h3 className="text-lg font-bold uppercase mb-1 text-orange-500">
+    VJC Overseas
+  </h3>
+  <p className="text-lg text-orange-500 font-bold -mt-2 ">(USA)</p>
+  <p className="mt-2">7200 Preston Rd, Plano,</p>
+<p>TX 75024, USA</p>
+<p className="mt-2 flex items-center justify-center space-x-2 hover:text-saffron">
+    <FaWhatsapp size={25} />
+    <a href="tel:+13322012159" className="text-base">
+        +1 3322012159
+    </a>
+</p>
+
             {/* Google Map */}
             <div className="mt-6">
                 <iframe
@@ -145,8 +148,11 @@ const Footer = () => {
 
         {/* Bangalore Address */}
         <div className="mb-8 md:mb-0 text-center md:w-1/3">
-            <h3 className="text-lg font-bold uppercase mb-2 text-orange-500">VJC Overseas (Bangalore)</h3>
-            <p>16 & 17, Ground Floor,</p>
+  <h3 className="text-lg font-bold uppercase mb-1 text-orange-500">
+    VJC Overseas
+  </h3>
+  <p className="text-lg text-orange-500 font-bold -mt-2 ">(BANGALORE)</p>
+            <p className='mt-2'>16 & 17, Ground Floor,</p>
             <p>Raheja Arcade,</p>
             <p>5th Block, Koramangala,</p>
             <p>Bangalore, India, 560095</p>
